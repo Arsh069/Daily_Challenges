6efb26d1287809df5b77d1d674177022efea76b0
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-10-08 23:19:04 +0530

    October 7 python solution

---
+++ 732. My Calendar III.py
@@ -0,0 +1,49 @@
+"""
+A k-booking happens when k events have some non-empty intersection (i.e., there is some time that is common to all k events.)
+
+You are given some events [start, end), after each given event, return an integer k representing the maximum k-booking between all the previous events.
+
+Implement the MyCalendarThree class:
+
+MyCalendarThree() Initializes the object.
+int book(int start, int end) Returns an integer k representing the largest integer such that there exists a k-booking in the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendarThree", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, 1, 1, 2, 3, 3, 3]
+
+Explanation
+MyCalendarThree myCalendarThree = new MyCalendarThree();
+myCalendarThree.book(10, 20); // return 1, The first event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+myCalendarThree.book(50, 60); // return 1, The second event can be booked and is disjoint, so the maximum k-booking is a 1-booking.
+myCalendarThree.book(10, 40); // return 2, The third event [10, 40) intersects the first event, and the maximum k-booking is a 2-booking.
+myCalendarThree.book(5, 15); // return 3, The remaining events cause the maximum K-booking to be only a 3-booking.
+myCalendarThree.book(5, 10); // return 3
+myCalendarThree.book(25, 55); // return 3
+
+"""
+
+class MyCalendarThree:
+
+    def __init__(self):
+        self.max=0
+        self.prev=[]
+
+    def book(self, start: int, end: int) -> int:
+        insort(self.prev,(start,1))
+        insort(self.prev,(end,-1))
+        count=0
+        for x,delta in self.prev:
+            count+=delta
+            self.max=max(self.max,count)
+        return self.max    
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
